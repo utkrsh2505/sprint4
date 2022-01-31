c01175211f0ce12cfa4f6883b63415736d717a56
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2022-01-31 22:02:29 +0530

    update

--- coding/mock/display.js
+++ coding/mock/display.js
@@ -14,9 +14,15 @@ const disDiv =(data)=>{
     let box = document.createElement("div");
     box.className = "item_box";
     let book = document.createElement("h2");
-    book.textContent = `${data.book}`
+    book.textContent = ` Book Name :${data.book}`
+    let author = document.createElement("h4");
+    author.textContent = ` Author : ${data.author}`
+    
+    let comments = document.createElement("h4");
+    comments.textContent = ` Comments : ${data.comments}`
+    
 
-    box.append(book);
+    box.append(book,author,comments);
     return box;
 
 }
